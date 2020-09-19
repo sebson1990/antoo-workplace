3f97796700d36cc295122ac1348f2b9682224734
Seba <Seba@Sebastians-MacBook-Pro.local>
2020-09-19 20:24:49 +0100

    added port and ip

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ def hello():
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
+        port=(os.environ.get('PORT')),
         debug=True)
